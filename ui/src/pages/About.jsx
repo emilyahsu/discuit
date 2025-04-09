7634edf3cc8ce0792cf9660f00ec0f6323acdd57
Emily Hsu <emilyahsu@stanford.edu>
2025-04-08 21:39:48 -0700

    revert changes to about

--- ui/src/pages/About.jsx
+++ ui/src/pages/About.jsx
@@ -5,72 +5,77 @@ import StaticPage from '../components/StaticPage';
 const About = () => {
   const faqItems = [
     {
-      question: 'How does Seddit make money?',
+      question: 'How does Discuit make money?',
       answer: (
         <>
-          {'We don't. Seddit is funded entirely through donations from our users via our '}
-          <a href="https://www.patreon.com/seddit" target="_blank" rel="noreferrer">
-            Patreon page
+          {'We don’t. Discuit is funded entirely through donations from our users via our '}
+          <a href="https://www.patreon.com/discuit" target="_blank" rel="noreferrer">
+            Patreon
           </a>
           .
         </>
       ),
     },
     {
-      question: 'Will Seddit have video uploads?',
+      question: 'Would a non-profit social platform work at scale?',
+      answer: `
+        We do not know if all types of social platforms can have their non-profit
+        alternatives—a non-profit alternative to Youtube probably won’t work, as
+        hosting video is highly expensive—but we’re quite confident that Discuit can
+        scale into millions of users just fine as a non-profit.  Hosting text is
+        extremely cheap; images are not too costly either; it’s video that is
+        expensive, and Discuit does not, nor do we plan to, have native video uploads.
+      `,
+    },
+    {
+      question: 'Is Discuit a federated platform?',
       answer: (
         <>
-          {'No. While we do plan to support video embeds from platforms like YouTube, '}
-          hosting video is highly expensive—but we're quite confident that Seddit can
-          handle video embeds from other platforms. Video hosting, on the other hand, is
-          expensive, and Seddit does not, nor do we plan to, have native video uploads.
+          {`Discuit is not a federated social platform, and we do not plan to support
+            federation in the future either. This is because we do not believe that
+            federated platforms, for a few specific reasons, have a chance of becoming
+            mainstream social platforms one day.`}
         </>
       ),
     },
     {
-      question: 'Is Seddit a federated platform?',
+      question: 'Does Discuit have a mobile app?',
       answer: (
         <>
-          {`Seddit is not a federated social platform, and we do not plan to support
-          federation in the future. We believe that federation, while a noble goal,
-          often leads to a worse user experience and more complex moderation challenges.`}
+          {`At the moment, Discuit does not have an official iOS or Android app. But we do
+            have an official Progressive Web App (PWA) that can be installed on both iOS
+            and Android devices (with support for device notifications). We also have two
+            actively maintained third party apps developed by two of our users: `}
+          <Link to="/DiscoApp">Disco</Link> for iOS, and <Link to="/Diskette">Diskette</Link> for
+          Android.
         </>
       ),
     },
     {
-      question: 'Does Seddit have a mobile app?',
+      question: 'How can I contact someone at Discuit?',
       answer: (
         <>
-          {`At the moment, Seddit does not have an official iOS or Android app. But we do
-          have a Progressive Web App (PWA) that you can install on your phone. To install
-          it, visit Seddit on your phone's browser and you should see a prompt to install
-          the app. If you don't see the prompt, you can also add it to your home screen
-          manually.`}
+          You can send an email to{' '}
+          <a href={`mailto:${import.meta.env.VITE_EMAILCONTACT}`}>
+            {import.meta.env.VITE_EMAILCONTACT}
+          </a>
+          , or join our <a href={import.meta.env.VITE_DISCORDURL}>Discord server </a>
+          (after you join, create a ticket to contact an admin).
         </>
       ),
     },
     {
-      question: 'How can I contact someone at Seddit?',
+      question: 'I have feedback, bug reports, or questions. What should I do?',
       answer: (
         <>
-          {'You can reach us at '}
-          <a href="mailto:contact@seddit.org">contact@seddit.org</a>
-          {'. We try to respond to all emails within 24 hours. If you're reporting a bug,
-          please include as much detail as possible, including your browser and operating
-          system.'}
-        </>
-      ),
-    },
-    {
-      question: 'Where can I ask questions about Seddit?',
-      answer: (
-        <>
-          {`If you have a question about Seddit that's not addressed here, feel free to
-          ask in our `}
-          <Link to="/SedditMeta">SedditMeta</Link>
-          {' or '}
-          <Link to="/SedditSuggestions">SedditSuggestions</Link>
-          {' communities.'}
+          {`If you have a question about Discuit that’s not addressed here, feel free to
+        ask it in the `}
+          <Link to="/DiscuitMeta">DiscuitMeta</Link>
+          {` community. If you have feedback or would like to report a bug, you can create a post in the `}
+          <Link to="/DiscuitSuggestions">DiscuitSuggestions</Link>
+          {` community (if you have a GitHub account, however, the best place to report a bug would be on `}
+          <a href={`${import.meta.env.VITE_GITHUBURL}/issues`}>GitHub</a>
+          {`)`}.
         </>
       ),
     },
@@ -116,9 +121,8 @@ const About = () => {
             A social platform by the users, for the users.
           </h1>
           <h2 className="about-subheading">
-            Seddit is a non-profit, open-source community discussion platform. It's an alternative
-            to traditional social media platforms, focusing on community-driven discussions
-            and content moderation.
+            Discuit is a non-profit, open-source community discussion platform. It’s an alternative
+            to Reddit.
           </h2>
         </div>
         <div className="squiggly-line"></div>
@@ -129,13 +133,13 @@ const About = () => {
             <p>
               Our mission is to build the first large-scale social media platform where the
               interests of the platform are aligned with the interests of the user—a platform, in
-              other words, that's built on principles of ethical design. At the heart of these
+              other words, that’s built on principles of ethical design. At the heart of these
               principles is the idea of giving users the freedom to choose their online social
               experience as they would prefer.
             </p>
             <p>
               Social media platforms have hitherto done the opposite and taken away what little
-              control the users had, as it served those companies' own self-interest, which was and
+              control the users had, as it served those companies’ own self-interest, which was and
               still remains, to make as much money as possible, without any regard for, indeed to
               the utter detriment of, the well-being of the user.
             </p>
@@ -150,12 +154,12 @@ const About = () => {
             <div className="about-highlight">
               <span className="is-bold">No ads. No tracking.</span>
               There are no ads, no forms of affiliate marketing, and no tracking anywhere on
-              Seddit. And neither your attention, nor your data, is monetized in any way, shape or
+              Discuit. And neither your attention, nor your data, is monetized in any way, shape or
               form.
             </div>
             <div className="about-highlight">
               <span className="is-bold">Enshitification-proof.</span>
-              {`Seddit is a non-profit that's funded entirely by its users through donations. The
+              {`Discuit is a non-profit that's funded entirely by its users through donations. The
               lack of a profit motive—and the lack of any shareholders or investors to answer to—is
               essential in keeping this platform completely aligned with the interests and the
               well-being of its users.`}
@@ -163,15 +167,15 @@ const About = () => {
             <div className="about-highlight">
               <span className="is-bold">Giving agency to users.</span>
               Choice over what appears on your feed. Multiple feeds. A plethora of ways to filter
-              content. In short, you have complete control over what you see on Seddit. (Please
-              note that Seddit is a work in progress and that many of these features are yet to be
+              content. In short, you have complete control over what you see on Discuit. (Please
+              note that Discuit is a work in progress and that many of these features are yet to be
               built.)
             </div>
             <div className="about-highlight">
               <span className="is-bold">No dark patterns.</span>
-              On Seddit, there are no nagging popups asking you to sign up. You don't need an
+              On Discuit, there are no nagging popups asking you to sign up. You don’t need an
               account to simply view a page. Images, in their highest quality, can be freely
-              downloaded. We don't manipulate you into using our platform more than you desire to.
+              downloaded. We don’t manipulate you into using our platform more than you desire to.
             </div>
           </div>
           <div className="about-section about-faq">
