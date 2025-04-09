87632a7df80d936d15a363555f0c815f11b65d84
Emily Hsu <emilyahsu@stanford.edu>
2025-04-08 21:28:56 -0700

    change request community to create community

--- ui/src/pages/AllCommunities.jsx
+++ ui/src/pages/AllCommunities.jsx
@@ -1,34 +1,28 @@
 import clsx from 'clsx';
-import PropTypes from 'prop-types';
 import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import Button, { ButtonClose } from '../components/Button';
+import Button from '../components/Button';
 import CommunityProPic from '../components/CommunityProPic';
 import Dropdown from '../components/Dropdown';
 import Feed from '../components/Feed';
-import { FormField } from '../components/Form';
-import Input, { InputWithCount, useInputMaxLength } from '../components/Input';
 import MarkdownBody from '../components/MarkdownBody';
 import MiniFooter from '../components/MiniFooter';
-import Modal from '../components/Modal';
 import ShowMoreBox from '../components/ShowMoreBox';
 import Sidebar from '../components/Sidebar';
-import { communityNameMaxLength } from '../config';
-import { mfetch, mfetchjson } from '../helper';
-import { useInputUsername } from '../hooks';
+import { mfetchjson } from '../helper';
 import { FeedItem } from '../slices/feedsSlice';
 import {
   allCommunitiesSearchQueryChanged,
   allCommunitiesSortChanged,
-  loginPromptToggled,
-  snackAlert,
   snackAlertError,
+  createCommunityModalOpened,
 } from '../slices/mainSlice';
 import { SVGClose, SVGSearch } from '../SVGs';
 import LoginForm from '../views/LoginForm';
 import JoinButton from './Community/JoinButton';
 import { isInfiniteScrollingDisabled } from './Settings/devicePrefs';
+import CreateCommunity from '../components/CreateCommunity';
 
 const prepareText = (isMobile = false) => {
   const x = isMobile ? 'by filling out the form below' : 'by clicking on the button below';
@@ -132,9 +126,12 @@ const AllCommunities = () => {
             />
             {!isSearching && renderSortDropdown()}
             {!isSearching && (
-              <RequestCommunityButton className="button-main is-m comms-new-button" isMobile>
+              <button 
+                className="button-main is-m comms-new-button" 
+                onClick={() => dispatch(createCommunityModalOpened())}
+              >
                 New
-              </RequestCommunityButton>
+              </button>
             )}
           </div>
         </div>
@@ -163,125 +160,24 @@ const AllCommunities = () => {
 
 AllCommunities.propTypes = {};
 
-export default AllCommunities;
-
 const CommunityCreationCard = () => {
-  return (
-    <div className="card card-sub card-padding home-welcome">
-      <div className="home-welcome-join">New communities</div>
-      <div className="home-welcome-subtext">{prepareText()}</div>
-      <div className="home-welcome-buttons">
-        <RequestCommunityButton className="button-main">Request a community</RequestCommunityButton>
-      </div>
-    </div>
-  );
-};
-
-const RequestCommunityButton = ({ children, isMobile = false, ...props }) => {
-  const loggedIn = useSelector((state) => state.main.user) !== null;
-  const dispatch = useDispatch();
-
   const [open, setOpen] = useState(false);
   const handleClose = () => setOpen(false);
 
-  const noteLength = 500;
-
-  const [formError, setFormError] = useState('');
-
-  const [name, handleNameChange] = useInputUsername(communityNameMaxLength);
-  const [note, handleNoteChange] = useInputMaxLength(noteLength);
-
-  useEffect(() => {
-    setFormError('');
-  }, [name, note]);
-
-  const handleButtonClick = () => {
-    if (!loggedIn) {
-      dispatch(loginPromptToggled());
-      return;
-    }
-    setOpen(true);
-  };
-
-  const handleSubmit = async () => {
-    if (name.length < 3) {
-      alert('Community name has to have at least 3 characters.');
-      return;
-    }
-    try {
-      const res = await mfetch(`/api/community_requests`, {
-        method: 'POST',
-        body: JSON.stringify({
-          name,
-          note,
-        }),
-      });
-      if (res.ok) {
-        dispatch(snackAlert('Requested!'));
-        handleClose();
-      } else {
-        if (res.status === 409) {
-          setFormError('A community by that name already exists');
-        } else {
-          throw new Error(await res.text());
-        }
-      }
-    } catch (error) {
-      dispatch(snackAlertError(error));
-    }
-  };
-
   return (
-    <>
-      <Modal open={open} onClose={handleClose}>
-        <div className="modal-card modal-form modal-request-comm">
-          <div className="modal-card-head">
-            <div className="modal-card-title">Request community</div>
-            <ButtonClose onClick={handleClose} />
-          </div>
-          <div className="form modal-card-content flex-column inner-gap-1">
-            <div className="form-field">{isMobile && <p>{prepareText(true)}</p>}</div>
-            <FormField label="Community name" description="Community name cannot be changed.">
-              <InputWithCount
-                value={name}
-                onChange={handleNameChange}
-                maxLength={communityNameMaxLength}
-                style={{ marginBottom: '0' }}
-                autoFocus
-              />
-            </FormField>
-            <FormField label="Note" description="An optional message for the admins.">
-              <InputWithCount
-                value={note}
-                onChange={handleNoteChange}
-                textarea
-                rows="4"
-                maxLength={noteLength}
-              />
-            </FormField>
-            {formError !== '' && (
-              <div className="form-field">
-                <div className="form-error text-center">{formError}</div>
-              </div>
-            )}
-            <FormField>
-              <button className="button-main" onClick={handleSubmit} style={{ width: '100%' }}>
-                Request community
-              </button>
-            </FormField>
-          </div>
-        </div>
-      </Modal>
-      <button onClick={handleButtonClick} {...props}>
-        {children}
-      </button>
-    </>
+    <div className="card card-sub card-padding home-welcome">
+      <div className="home-welcome-join">New communities</div>
+      <div className="home-welcome-subtext">
+        Create a new community to discuss topics you're passionate about.
+      </div>
+      <div className="home-welcome-buttons">
+        <button className="button-main" onClick={() => setOpen(true)}>
+          Create a community
+        </button>
+      </div>
+      <CreateCommunity open={open} onClose={handleClose} />
+    </div>
   );
-};
-
-RequestCommunityButton.propTypes = {
-  isMobile: PropTypes.bool,
-  children: PropTypes.node.isRequired,
 };
 
 const ListItem = React.memo(function ListItem({ community }) {
